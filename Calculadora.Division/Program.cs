d3581336aaef16e00808fcd0a65ec0503da64ca5
Nezmesis093 <105186809+Nezmesis093@users.noreply.github.com>
2023-06-03 06:36:26 -0600

    Initial commit

---
+++ Calculadora.Division/Program.cs
@@ -0,0 +1,26 @@
+﻿//Calculadora de Division de dos valores.
+
+// Declaracion (inicializacion) de Variables:
+
+// Ejemplo practico [ tipo de dato(numerico, cadena, boolean) ] + [nombre de la variable]
+
+double ValorA = 0.0;
+double ValorB = 0.0;
+double Total = 0.0;
+
+// Obtenermos los datos a Sumar del ValorA y ValorB desde la consola:
+
+Console.WriteLine("Escribe número o el valor a Dividir: "); //Mostrar a Usuario instruccion a realizar
+ValorA = Convert.ToDouble(Console.ReadLine()); //Realiza la asignacion o nuevo valor a la Variable ValorA con la instruccion ReadLine para despues convertir el valor obtenido a un Valor Double con el comando Convert.To
+
+Console.WriteLine("Escribe número o el valor por el cual Dividir: ");
+ValorB = Convert.ToDouble(Console.ReadLine());
+
+// Realizamos el proceso o la intruccion a realizar con los datos obtenidos:
+
+Total = ValorA / ValorB;
+
+// Mostrar el Resultado en consola:
+
+Console.WriteLine("El Resultado Total de la DIVISION es de: " + Total); // Mostramos a Usuario el texto de resultado e incluimos en Valor Total a lo mostrado en consola
+Console.ReadKey();
